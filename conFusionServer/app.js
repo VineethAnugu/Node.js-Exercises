e1c066b119f40c945c5f59e3227ab8c84b36179c
VineethAnugu <a.vineethreddy575@gmail.com>
2019-07-14 12:13:42 -0400

    Express session part 1

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -29,13 +31,22 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-abcd-efgh'));
+//app.use(cookieParser('12345-67890-abcd-efgh'));   for cookies
+
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
+
 
 //basic authorization
 function auth(req, res, next){
-  console.log(req.signedCookies);
+  console.log(req.session);
 
-  if(!req.signedCookies.user){
+  if(!req.session.user){
     var authHeader = req.headers.authorization;
 
     if(!authHeader){
@@ -50,7 +61,7 @@ function auth(req, res, next){
     var password = auth[1];
   
     if(username === 'admin' && password === 'password'){
-      res.cookie('user', 'admin', {signed: true});
+      req.session.user = 'admin';
       next();
     }
     else{
@@ -61,7 +72,7 @@ function auth(req, res, next){
     }
   }
   else{
-    if(req.signedCookies.user === 'admin'){
+    if(req.session.user === 'admin'){
       next();
     }
     else{
@@ -71,8 +82,6 @@ function auth(req, res, next){
       next(err);
     }
   }
-
-
 }
 
 app.use(auth);
